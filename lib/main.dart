5dc377613d07821161e515389099bc4950a59e66
Kuloud <kuloud@outlook.com>
2023-10-16 17:13:57 +0800

    feat: add geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_my_tracker/app.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+// import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+// import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 import 'dart:async';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final GoogleMapsFlutterPlatform mapsImplementation =
-      GoogleMapsFlutterPlatform.instance;
-  if (mapsImplementation is GoogleMapsFlutterAndroid) {
-    mapsImplementation.useAndroidViewSurface = true;
-    initializeMapRenderer();
-  }
+  // final GoogleMapsFlutterPlatform mapsImplementation =
+  //     GoogleMapsFlutterPlatform.instance;
+  // if (mapsImplementation is GoogleMapsFlutterAndroid) {
+  //   mapsImplementation.useAndroidViewSurface = true;
+  //   initializeMapRenderer();
+  // }
 
   runApp(MyTrackerApp());
 }
 
-Completer<AndroidMapRenderer?>? _initializedRendererCompleter;
+// Completer<AndroidMapRenderer?>? _initializedRendererCompleter;
 
-Future<AndroidMapRenderer?> initializeMapRenderer() async {
-  if (_initializedRendererCompleter != null) {
-    return _initializedRendererCompleter!.future;
-  }
+// Future<AndroidMapRenderer?> initializeMapRenderer() async {
+//   if (_initializedRendererCompleter != null) {
+//     return _initializedRendererCompleter!.future;
+//   }
 
-  final Completer<AndroidMapRenderer?> completer =
-      Completer<AndroidMapRenderer?>();
-  _initializedRendererCompleter = completer;
+//   final Completer<AndroidMapRenderer?> completer =
+//       Completer<AndroidMapRenderer?>();
+//   _initializedRendererCompleter = completer;
 
-  WidgetsFlutterBinding.ensureInitialized();
+//   WidgetsFlutterBinding.ensureInitialized();
 
-  final GoogleMapsFlutterPlatform mapsImplementation =
-      GoogleMapsFlutterPlatform.instance;
-  if (mapsImplementation is GoogleMapsFlutterAndroid) {
-    unawaited(mapsImplementation
-        .initializeWithRenderer(AndroidMapRenderer.latest)
-        .then((AndroidMapRenderer initializedRenderer) =>
-            completer.complete(initializedRenderer)));
-  } else {
-    completer.complete(null);
-  }
+//   final GoogleMapsFlutterPlatform mapsImplementation =
+//       GoogleMapsFlutterPlatform.instance;
+//   if (mapsImplementation is GoogleMapsFlutterAndroid) {
+//     unawaited(mapsImplementation
+//         .initializeWithRenderer(AndroidMapRenderer.latest)
+//         .then((AndroidMapRenderer initializedRenderer) =>
+//             completer.complete(initializedRenderer)));
+//   } else {
+//     completer.complete(null);
+//   }
 
-  return completer.future;
-}
+//   return completer.future;
+// }
--- lib/pages/index/index_page.dart
+++ lib/pages/index/index_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_my_tracker/generated/l10n.dart';
+import 'package:flutter_my_tracker/pages/map/geo_location.dart';
 import 'package:flutter_my_tracker/pages/map/main_ui.dart';
+import 'package:flutter_my_tracker/pages/map/map_page.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -10,25 +12,59 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+  int _selectedIndex = 0;
+
+  final _pages = <Widget>[
+    const MapPage(),
+    const MapPage(),
+    const MapPage(),
+    GeolocatorWidget()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapUiPage(),
-      bottomNavigationBar:
-          BottomNavigationBar(useLegacyColorScheme: false, items: [
-        BottomNavigationBarItem(
-            icon: const Icon(Icons.map),
-            label: S.of(context).bottomNavigationBarLabelMap),
-        BottomNavigationBarItem(
-            icon: const Icon(Icons.home),
-            label: S.of(context).bottomNavigationBarLabelMap),
-        BottomNavigationBarItem(
-            icon: const Icon(Icons.track_changes),
-            label: S.of(context).bottomNavigationBarLabelTracks),
-        BottomNavigationBarItem(
-            icon: const Icon(Icons.history),
-            label: S.of(context).bottomNavigationBarLabelHistory),
-      ]),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          useLegacyColorScheme: false,
+          showUnselectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.map),
+                label: S.of(context).bottomNavigationBarLabelMap),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.home),
+                label: S.of(context).bottomNavigationBarLabelMap),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.track_changes),
+                label: S.of(context).bottomNavigationBarLabelTracks),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.history),
+                label: S.of(context).bottomNavigationBarLabelHistory),
+          ]),
+      // floatingActionButton: Column(
+      //   mainAxisSize: MainAxisSize.min,
+      //   children: [
+      //     FloatingActionButton(
+      //       onPressed: () {},
+      //       child: const Icon(Icons.add),
+      //     ),
+      //     const SizedBox(
+      //       height: 16,
+      //       width: 16,
+      //     ),
+      //     FloatingActionButton(
+      //       onPressed: () {},
+      //       child: const Icon(Icons.add),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
